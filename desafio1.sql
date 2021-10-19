1b6366b3e2351e71e25936dcdc3d63a8a3b2eee1
Wesley Maia <maia.weol@gmail.com>
2021-10-19 16:11:28 -0300

    Requisito 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT product_name FROM northwind.products;
 -- SET GLOBAL validate_password.policy=LOW;
 -- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'trybe123';
--- flush privileges; 
+-- flush privileges;
+SELECT product_name FROM northwind.products;
