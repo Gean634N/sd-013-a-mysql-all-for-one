5498825558067d44a0122305056bd6fec795545d
Gustavo Mauricio <gmauricio207@gmail.com>
2021-10-19 16:09:08 -0300

    Req 14 e 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,4 +1,3 @@
 -- Mostre somente as horas (sem os minutos e os segundos) da coluna submitted_date de todos registros da tabela purchase_orders.
-SELECT HOUR(submitted_date) FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_hour FROM northwind.purchase_orders;
 
--- SELECT CURTIME() AS HORA, TIME_FORMAT(CURTIME(), '%h - %i - %s') AS HORA_FORMATADA;
