db018fab44d9fc8212c2a03e732c980c88bbc9e3
Rafael Victor <rafavictorgs@gmail.com>
2021-10-19 15:37:08 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,3 @@
+SELECT notes FROM northwind.purchase_orders WHERE NOT notes = 'null';
+-- SELECT notes FROM northwind.purchase_orders WHERE notes != 'null';
+-- SELECT notes FROM northwind.purchase_orders WHERE notes <> 'null';
