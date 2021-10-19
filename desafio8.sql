3ab990b6559f8ae7aa5ccea6b881d0eada806639
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-10-19 15:53:03 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,4 @@
+-- primeira coluna mostra 5 + 6; chamar "A"
+-- segunda coluna a palavra "de"; chamar "Trybe"
+-- terceira coluna exiba 2 + 8; chamar "eh"
+SELECT (5 + 6) AS 'A', 'de' AS 'Trybe',(2 + 8) AS 'eh';
